567286904dfdabe1f2c2393873b7fcc25c105540
kwdev2k7 <kwdev2k7@gmail.com>
2020-10-03 22:21:40 +0300

    Simplier - better.

--- index.js
+++ index.js
@@ -5,26 +5,12 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
-let replies = [
-	"Pong!",
-	"ping pong, gong kong",
-	"лови пингово понговый мяч в лицо"
-];
-
 let authors = {
 	"kwdev2k7": "developer.",
 	"doshirak": "ideas.",
 	"LEON": "ideas.",
 	"Demetry": "ideas."
 }
-
-let gifs = [
-	"https://cdn.discordapp.com/attachments/696409263727181854/761683813624840222/pingpong.gif",
-	"https://cdn.discordapp.com/attachments/696409263727181854/761685682393448459/pingpong1.gif",
-	"https://cdn.discordapp.com/attachments/696409263727181854/761685683122733066/pingpong2.gif",
-	"https://cdn.discordapp.com/attachments/696409263727181854/761880544526598154/pingpong3.jpg"
-]
 
 function timeformat(ms){
 	let s = parseInt(ms / 1000);
@@ -37,10 +23,10 @@ function timeformat(ms){
 
 client.on('message', msg => {
   if (msg.content === '!ping' || msg.content === "!p") {
-    msg.reply(':ping_pong: '+replies[Math.floor(Math.random()*replies.length)]+"\n--- Random 3 authors: \n"+Object.keys(authors).map(key=>[key, authors[key]]).sort(()=>Math.random()-Math.random()).slice(0,3).map(author=>author.join(": ")).join("\n")+"\n---\n"+gifs[Math.floor(Math.random()*gifs.length)]);
+    msg.channel.send(":ping_pong: Pong!");
   }
   if(msg.content === "!info" || msg.content === "!i"){
-  	msg.reply("\n---\n"+"Guilds: "+client.guilds.cache.size+".\nUsers: "+client.guilds.cache.map(g=>g.members.cache.size).reduce((a,b)=>a+b)+".\nUptime: "+timeformat(Date.now()-client.readyAt)+".\nAuthors of this masterpiece:\n"+Object.keys(authors).map(key=>[key, authors[key]]).map(author=>author.join(": ")).join("\n")+"\n---");
+  	msg.channel.send("\n---\n"+"Guilds: "+client.guilds.cache.size+".\nUsers: "+client.guilds.cache.map(g=>g.members.cache.size).reduce((a,b)=>a+b)+".\nUptime: "+timeformat(Date.now()-client.readyAt)+".\nAuthors of this masterpiece:\n"+Object.keys(authors).map(key=>[key, authors[key]]).map(author=>author.join(": ")).join("\n")+"\n---");
   }
 });
 
