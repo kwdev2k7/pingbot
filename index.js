aa4f238e2568b3aed13f3a9fe832f0d1902024d6
kwdev2k7 <kwdev2k7@gmail.com>
2020-10-03 13:25:49 +0300

    Add info command.

--- index.js
+++ index.js
@@ -26,9 +26,21 @@ let gifs = [
 	"https://cdn.discordapp.com/attachments/696409263727181854/761880544526598154/pingpong3.jpg"
 ]
 
+function timeformat(ms){
+	let s = parseInt(ms / 1000);
+	let m = parseInt(s / 60);
+	s = s % 60;
+	let h = parseInt(m / 60);
+	m = m % 60;
+	return (h<10?"0"+h:h)+":"+(m<10?"0"+m:m)+":"+(s<10?"0"+s:s);
+}
+
 client.on('message', msg => {
   if (msg.content === '!ping' || msg.content === "!p") {
     msg.reply(':ping_pong: '+replies[Math.floor(Math.random()*replies.length)]+"\n--- Random 3 authors: \n"+Object.keys(authors).map(key=>[key, authors[key]]).sort(()=>Math.random()-Math.random()).slice(0,3).map(author=>author.join(": ")).join("\n")+"\n---\n"+gifs[Math.floor(Math.random()*gifs.length)]);
+  }
+  if(msg.content === "!info" || msg.content === "!i"){
+  	msg.reply("\n---\n"+"Guilds: "+client.guilds.cache.size+".\nUsers: "+client.guilds.cache.map(g=>g.members.cache.size).reduce((a,b)=>a+b)+".\nUptime: "+timeformat(Date.now()-client.readyAt)+".\nAuthors of this masterpiece:\n"+Object.keys(authors).map(key=>[key, authors[key]]).map(author=>author.join(": ")).join("\n")+"\n---");
   }
 });
 
